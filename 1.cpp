59f9b94545d586e3b0e1aed305830e51ddd610a2
2093560892 <2093560892@qq.com>
2024-01-11 15:11:01 +0800

    提交代码1.cpp

---
+++ 1.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+using namespace std;
+
+typedef int Status;
+typedef char TElemType;
+typedef struct BiTNode{
+	TElemType data;
+	struct BiTNode *lchild,*rchild;
+}BiTNode,*BiTree;
+
+Status InitBiTree(BiTree &T){
+	T=(BiTree)malloc(sizeof(BiTNode));
+	if(!T) exit(EOVERFLOW);
+	T->data='0';
+	T->lchild=T->rchild=NULL;
+	return OK;
+}
+
+Status CreateBiTree(BiTree &T){
+	BiTree p;
+	BiTree q1,q2;
+	queue<BiTree>Q;
+	TElemType data;
+	Q.push(T);
+	while(!Q.empty()){
+		p=Q.front();
+		scanf("%c",&data);
+		getchar();
+		if(data=='#') break;
+		p->data=data;
+		Q.pop();
+		InitBiTree(q1);
+		p->lchild=q1;
+		Q.push(p->lchild);
+		InitBiTree(q2);
+		p->rchild=q2;
+		Q.push(p->rchild);
+	}
+	return OK;
+}
+
+Status num_root(BiTree T){
+	if(T->data=='0'){
+		return 0;
+	}
+	if(T->lchild->data=='0'&&T->rchild->data=='0'){
+		return 1;
+	}
+	else{
+		return num_root(T->lchild)+num_root(T->rchild);
+	}
+}
+
+int main(){
+	int n,i=0;
+	BiTree T;
+	InitBiTree(T);
+	scanf("%d",&n);
+	getchar();
+	CreateBiTree(T);
+	i=num_root(T);
+	printf("%d",n-i);
+	return 0;
+}/*7
+A B C D E 0 0 0 0 F G #*/
